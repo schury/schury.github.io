8791a79c4a738f9a5d43add607834a182dad2541
Fabian Schury <fab.schury@gmail.com>
2022-01-03 12:06:28 +0100

    Gallery10

--- gallery.js
+++ gallery.js
@@ -1,3 +1,4 @@
+/*
 var gallery = document.querySelector("#gallery");
 var getVal = function (elem, style) { return parseInt(window.getComputedStyle(elem).getPropertyValue(style)); };
 var getHeight = function (item) { return item.querySelector(".content").getBoundingClientRect().height; };
@@ -9,26 +10,10 @@ var resizeAll = function () {
         el.style.gridRowEnd = "span " + Math.ceil((getHeight(item) + gap) / (altura + gap));
     });
 };
-gallery.querySelectorAll("img").forEach(function (item) {
-    item.classList.add("byebye");
-    if (item.complete) {
-        console.log(item.src);
-    }
-    else {
-        item.addEventListener("load", function () {
-            var altura = getVal(gallery, "grid-auto-rows");
-            var gap = getVal(gallery, "grid-row-gap");
-            var gitem = item.parentElement.parentElement;
-            gitem.style.gridRowEnd = "span " + Math.ceil((getHeight(gitem) + gap) / (altura + gap));
-            item.classList.remove("byebye");
-        });
-    }
-});
-window.addEventListener("resize", resizeAll);
-window.addEventListener("DOMContentLoaded", resizeAll);
-
-gallery.querySelectorAll(".gallery-item").forEach(function (item) {
-    item.addEventListener("click", function () {        
-        item.classList.toggle("full");        
-    });
-});
+*/
+// window.addEventListener("resize", resizeAll);
+// window.addEventListener("DOMContentLoaded", resizeAll);
+document.addEventListener('load', function() {
+  // your code here
+  window.alert("test");
+}, false);
