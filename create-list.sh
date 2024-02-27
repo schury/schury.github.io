019888dca798737c160ed28ca92f454f5dff9abf
Schury <f.schury@web.de>
2024-02-27 14:14:31 +0100

    raw list now also in python

--- create-list.sh
+++ create-list.sh
@@ -1,24 +1,48 @@
 #!/usr/bin/python3
 
 import os, platform
+import glob
 
 print(platform.node())
 
-directory = "songs"
-files = []
-for f in os.listdir(directory):
-  files.append(f)
+flac_directory = "/home/fab/ogg/new"
+curr_directory = os.getcwd()
+#files = []
+#for f in os.listdir(directory):
+  #files.append(f)
 
-files.sort()
+#files.sort()
+#print(files)
 
-for f in files:
-  with open("songs/" + f) as file:
-    for l in file:
-        if "<h1>" in l:
-          print(l.removeprefix("<h1>").removesuffix("</h1>\n"))
+os.chdir(flac_directory)
+#result = glob.glob(directory + '/**/*.flac', recursive=True)
+result = glob.glob('**/*.flac', recursive=True)
+#print(result)
+os.chdir(curr_directory)
+result.sort()
 
-infile = open("cd-list-raw-length")
+#for f in files:
+  #with open("songs/" + f) as file:
+    #for l in file:
+        #if "<h1>" in l:
+          #print(l.removeprefix("<h1>").removesuffix("</h1>\n"))
+
+infile = open('cd-list-raw-length')
 cd_list = infile.read()
+infile.close()
+# print(cd_list)
 
-if "Zandelle" in cd_list:
-  print("yes")
+outfile = open('cd-list-raw-length', 'a')
+for r in result:
+  song = r[0:-5]
+  if song not in cd_list:
+    print("adding " + song)
+    # here we now call metaflac to get the length of the song
+    t = 'metaflac --show-total-samples "' + flac_directory + '/' + r + '"'
+    # print(t)
+    l = os.popen('metaflac --show-total-samples "' + flac_directory + '/' + r + '"').read()
+    # print(l)
+    outfile.write(song + ' / ' + l)
+
+os.popen('sort cd-list-raw-length > ttttemp')
+os.popen('mv -f ttttemp cd-list-raw-length')
