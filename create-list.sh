cc8cf72d58071c84dffd809cd55eff65d33f4fe1
Fabian Schury <fab.schury@gmail.com>
2024-02-29 08:17:03 +0100

    small change in create-list

--- create-list.sh
+++ create-list.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
-import os, platform
-import glob
-
-print(platform.node())
+import os, sys, glob
 
 flac_directory = "/home/fab/ogg/new"
+if not os.path.isdir("flac_directory"):
+  print("flac directory not found - only works on hel")
+  sys.exit()
+
 curr_directory = os.getcwd()
 
 os.chdir(flac_directory)
@@ -19,13 +20,17 @@ cd_list = infile.read()
 infile.close()
 # print(cd_list)
 
+changes = False
+
 outfile = open('cd-list-raw-length', 'a')
 for r in result:
   song = r[0:-5]
   if song not in cd_list:
     print("adding " + song)
+    changes = True
     # here we now call metaflac to get the length of the song
     l = os.popen('metaflac --show-total-samples "' + flac_directory + '/' + r + '"').read()
     outfile.write(song + ' / ' + l)
 
-os.popen('sort cd-list-raw-length -o cd-list-raw-length')
+if changes:
+  os.popen('sort cd-list-raw-length -o cd-list-raw-length')
