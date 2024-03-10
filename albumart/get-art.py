b411160f6c7c41ec35c690c1c08167ff3a15e93f
Fabian Schury <fab.schury@gmail.com>
2024-03-10 14:09:58 +0100

    tiny change albumart

--- albumart/get-art.py
+++ albumart/get-art.py
@@ -10,17 +10,18 @@ import PIL
 
 # save a image using extension
 # im1 = im1.save("geeks.jpg")
-url = 'https://coverartarchive.org/release-group/0519b1bf-c443-3110-8fb0-6fbbf8f3862b/front'
+def download_and_save_art(rg_id):
+  url = 'https://coverartarchive.org/release-group/' + rg_id + '/front'
 
-with urllib.request.urlopen(url) as img:
-  print(img)
-  with open('out.jpg', 'wb') as outfile:
-    outfile.write(img.read())
-  # bla = Image(img).save('test.jpg')
+  with urllib.request.urlopen(url) as img:
+    print(img)
+    with open('out.jpg', 'wb') as outfile:
+      outfile.write(img.read())
+    # bla = Image(img).save('test.jpg')
 
-sys.exit()
+relgroup_id = '0519b1bf-c443-3110-8fb0-6fbbf8f3862b'
 
-artist = 'bolt+thrower'
+artist = 'bolt+thrower+honour+-+valour+-+pride'
 with urllib.request.urlopen('https://musicbrainz.org/search?query=' + artist + '&type=artist&limit=1&method=indexed') as artist_list:
   html_artist = str(artist_list.read()).split('\"')
   for h in html_artist: 
