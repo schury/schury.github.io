5e3486e865c5c4721fbf95ecb2236c54380a39a3
Fabian Schury <fab.schury@gmail.com>
2024-03-13 19:39:25 +0100

    small fix

--- albumart/get-art.py
+++ albumart/get-art.py
@@ -61,6 +61,7 @@ for cd in cdlist:
       first = False
     old_artist = artist
     searchstring = artist.replace(' ', '+')
+    searchstring = artist.replace('ö', 'o')
     with urllib.request.urlopen('https://musicbrainz.org/search?query=' + searchstring + '&type=artist&limit=1&method=indexed') as artist_list:
       html_artist = str(artist_list.read()).split('\"')
       for h in html_artist: 
