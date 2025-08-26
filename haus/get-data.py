8b62ce7c14775dfd353a9f08b228de0865bae359
Fabian Schury <fab.schury@gmail.com>
2025-08-26 17:29:08 +0200

    better formatting of output

--- haus/get-data.py
+++ haus/get-data.py
@@ -3,7 +3,7 @@
 import urllib.request, sys, os, json
 from urllib.error import HTTPError, URLError
 
-test = False
+test = True
 dateStart = '2025-01-01'
 dateEnd   = '2025-07-31'
 
@@ -20,23 +20,37 @@ url = raw_url.replace('$METERS', meters).replace('$APIKEY', api_key).replace('$D
 # for testing we use a local file containing the api response
 outstring = ''
 if test:
-  f = open('response', 'r')
+  f = open('response-2024', 'r')
   outstring = f.readline()
 else:
   data = urllib.request.urlopen(url)
   outstring = data.read().decode('UTF-8')
 
+type_header = '          '
+lines = []
+type_totals = '          '
+header_length = 0
+
 for i in range(len(types)):
   j = json.loads(outstring)["energyDetails"]["meters"]
   vals = j[i]["values"]
-  print(j[i]["type"])
+  header_length = len(type_header)
+  type_header = type_header + j[i]["type"] + '  '
   total = 0.0
+  k = 0
   for v in vals:
-    print(v["date"].split()[0] + '   ' + str(round(v["value"]/1000.0, 1)))
+    if i == 0:
+      lines.append(v["date"].split()[0][0:-3] + '   ' + str(round(v["value"]/1000.0, 1)))
+    else:
+      lines[k] = lines[k].ljust(header_length) + str(round(v["value"]/1000.0, 1))
+    k = k + 1
     total = total + v["value"]
-  print("Total: " + str(total/1000.0))
+  type_totals = type_totals.ljust(header_length) + str(round(total/1000.0, 1))
     
-
+print(type_header)
+for l in lines:
+  print(l)
+print(type_totals)
 
 if False:
   with urllib.request.urlopen(url) as data:
